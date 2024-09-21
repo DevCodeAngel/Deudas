4a01ce6635fa2e02fa05c89725ac086b690a1549
codecraftdev8@gmail.com <angeliglesias@gmail.com>
2024-09-21 11:56:08 -0500

    init

---
+++ app/Models/Cliente_Producto.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cliente_Producto extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'cliente_id',
+        'producto_id',
+        'cantidad',
+    ];
+}
